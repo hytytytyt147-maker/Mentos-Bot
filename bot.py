e26149c038a396ff7796a4c5f414a959fd00bbb6
hytytytyt147-maker <hytytytyt147@gmail.com>
2026-05-24 21:26:00 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-TOKEN = "8947024615:AAHf9RX5nl70knZ3aKy_4WRuhn5f83v4bIs"
+TOKEN = "8947024615:AAHf9RX5nl70knZ3aKy_4WRuhn5f83vHkIs"
 ADMIN_ID = 1924047464
 
 bot = Bot(token=TOKEN)
